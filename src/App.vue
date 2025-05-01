8b493c0ed6876a82df4c7cb7aedd697c12aec7c8
Edgar <edgaarr2005@hotmail.com>
2025-04-30 20:24:53 -0400

    Testing out animations...

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
-    <div class="block"></div>
-    <button>Animate</button>
+    <div class="block" :class="{animate: animatedBlock}"></div>
+    <button @click="animateBlock">Animate</button>
   </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
@@ -15,9 +15,15 @@
 <script>
 export default {
   data() {
-    return { dialogIsVisible: false };
+    return { 
+      animatedBlock: false,
+      dialogIsVisible: false 
+    };
   },
   methods: {
+    animateBlock() {
+      this.animatedBlock = !this.animatedBlock;
+    },
     showDialog() {
       this.dialogIsVisible = true;
     },
@@ -57,6 +63,7 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
+  transition: all 0.3s ease-out;
 }
 .container {
   max-width: 40rem;
@@ -68,5 +75,10 @@ button:active {
   padding: 2rem;
   border: 2px solid #ccc;
   border-radius: 12px;
+}
+.animate {
+  background-color: #e6ad11;
+  border-radius: 50%;
+  transform: translateX(-100px);
 }
 </style>
