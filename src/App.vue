b55cdd94ddb3452ad35361eae0446a231d619a6e
Edgar <edgaarr2005@hotmail.com>
2025-05-19 15:11:33 -0400

    Testing even more animations!

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div class="block" :class="{animate: animatedBlock}"></div>
+    <div class="block" :class="{'animate': animatedBlock, 'reverse-animate': !animatedBlock && reverseAnimatedBlock}"></div>
     <button @click="animateBlock">Animate</button>
   </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
@@ -17,12 +17,14 @@ export default {
   data() {
     return { 
       animatedBlock: false,
+      reverseAnimatedBlock: false,
       dialogIsVisible: false 
     };
   },
   methods: {
     animateBlock() {
       this.animatedBlock = !this.animatedBlock;
+      this.reverseAnimatedBlock = true;
     },
     showDialog() {
       this.dialogIsVisible = true;
@@ -63,7 +65,7 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
-  transition: all 0.3s ease-out;
+  /* transition: all 0.3s ease-out; */
 }
 .container {
   max-width: 40rem;
@@ -77,8 +79,45 @@ button:active {
   border-radius: 12px;
 }
 .animate {
-  background-color: #e6ad11;
+  /* background-color: #e6ad11;
   border-radius: 50%;
-  transform: translateX(-100px);
+  transform: translateX(-100px); */
+  animation: slide-zoom-circle-it 1s forwards;
+}
+
+.reverse-animate {
+  animation: reverse-slide-zoom-circle-it 1s forwards;
+}
+
+@keyframes slide-zoom-circle-it {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+  50% {
+    background-color: #e6ad11;
+    border-radius: 50%;
+    transform: translateX(-50px) scale(1.2);
+  }
+  100% {
+    background-color: #004f63;
+    border-radius: 20%;
+    transform: translateX(-100px) scale(0.75);
+  }
+}
+
+@keyframes reverse-slide-zoom-circle-it {
+  0% {
+    background-color: #004f63;
+    border-radius: 20%;
+    transform: translateX(-100px) scale(0.75);
+  }
+  50% {
+    background-color: #e6ad11;
+    border-radius: 50%;
+    transform: translateX(-50px) scale(1.2);
+  }
+  100% {
+    transform: translateX(0) scale(1);
+  }
 }
 </style>
